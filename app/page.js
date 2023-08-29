320108c9a91a473a0c1d2060b1c49c59ec6b11c7
Ivan <aw4ken1ngs@gmail.com>
2023-08-30 00:04:04 +0400

    google api

--- app/page.js
+++ app/page.js
@@ -2,12 +2,132 @@
 import * as React from 'react';
 import { OrderCard } from '@/components/order-card/order-card';
 import { MainInput } from '@/components/main-input';
+import Script from 'next/script';
+import { useEffect, useState } from 'react';
+import { keyframes } from '@nextui-org/react';
 
 export default function Home() {
   return (
     <>
-    <MainInput/>
-    <OrderCard/>
+      <Oaut />
+      <MainInput />
+      <OrderCard />
     </>
   )
 }
+let tokenClient = null;
+let gapiInited = false;
+let gisInited = false;
+const CLIENT_ID = '445389393504-965rm4qnvov159r3ek9h3sbva5et2pa4.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyA-og7qzrQ-aG8MSDB9jYEMuigHMrr5O2g';
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+
+const Oaut = () => {
+
+  const [authorizeButton, setAuthorizeButton] = useState(false);
+  const [signOutButton, setSignOutButton] = useState(false);
+      const [content, setContent] = useState('');
+
+
+  const handleAuthClick = () => {
+    tokenClient.callback = async (resp) => {
+      if (resp.error !== undefined) {
+        throw (resp);
+      }
+      setSignOutButton(true);
+      setAuthorizeButton(true);
+      await listMajors();
+    };
+
+    if (gapi.client.getToken() === null) {
+      // Prompt the user to select a Google Account and ask for consent to share their data
+      // when establishing a new session.
+      tokenClient.requestAccessToken({ prompt: 'consent' });
+    } else {
+      // Skip display of account chooser and consent dialog for an existing session.
+      tokenClient.requestAccessToken({ prompt: '' });
+    }
+  }
+  const handleSignoutClick = () => {
+    const token = gapi.client.getToken();
+    if (token !== null) {
+      google.accounts.oauth2.revoke(token.access_token);
+      gapi.client.setToken('');
+      setContent('');
+      setSignOutButton(false);
+    }
+  }
+
+  function maybeEnableButtons() {
+    if (gapiInited && gisInited) {
+      setAuthorizeButton(true);
+    }
+  }
+
+  function gisLoaded() {
+    tokenClient = google.accounts.oauth2.initTokenClient({
+      client_id: CLIENT_ID,
+      scope: SCOPES,
+      callback: '', // defined later
+    });
+    gisInited = true;
+    maybeEnableButtons();
+  }
+
+  function gapiLoaded() {
+    gapi.load('client', initializeGapiClient);
+  }
+
+  async function initializeGapiClient() {
+    await gapi.client.init({
+      apiKey: API_KEY,
+      discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true;
+    maybeEnableButtons();
+  }
+
+  async function listMajors() {
+    let response;
+    try {
+      // Fetch first 10 files
+      response = await gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+        range: 'Class Data!A2:E',
+      });
+    } catch (err) {
+      setContent(err.message);
+      return;
+    }
+    const range = response.result;
+    if (!range || !range.values || range.values.length == 0) {
+      setContent('No values found.');
+      return;
+    }
+    // Flatten to string to display
+    const output = range.values.reduce(
+      (str, row) => `${str}${row[0]}, ${row[4]}\n`,
+      'Name, Major:\n');
+    setContent(output)
+  }
+  return (
+    <>
+      {authorizeButton && <button onClick={handleAuthClick}>Authorize</button>}
+      {signOutButton && <button onClick={handleSignoutClick}>Sign Out</button>}
+      <pre id="content" style={{whiteSpace: 'pre-wrap'}}></pre>
+      <Script
+        src="https://apis.google.com/js/api.js"
+        strategy="lazyOnload"
+        onLoad={gapiLoaded}
+      />
+      <Script
+        src="https://accounts.google.com/gsi/client"
+        strategy="lazyOnload"
+        onLoad={gisLoaded}
+      />
+    </>
+  )
+}
+
+
--- components/order-card/order-card.jsx
+++ components/order-card/order-card.jsx
@@ -1,60 +1,132 @@
 import React from "react";
-import { Card, green, Text, Image, Col, Row, Spacer, Container } from "@nextui-org/react";
-import Mix from "@/images/mix.jpg"
+import { Card, green, Text, Image, Col, Row, Spacer, Container, Grid, } from "@nextui-org/react";
+import Mix from "@/images/mix.jpg";
+import Starina from "@/images/starina.jpg";
 import { CheckSquareIcon } from "@/images/icon/check-square-icon";
 import { XSquareIcon } from "@/images/icon/x-square-icon";
 import { BankIcon } from "@/images/icon/bank-icon";
 import { CalendarCheckIcon } from "@/images/icon/calendar-check-icon";
 
 export const OrderCard = () => {
-  return (
-    <Card css={{ mw: '333px', ml: '80px', mt: '115px' }}>
-      <Card.Header css={{ ai: 'start' }}>
-        <Image
-          src={Mix.src}
-          width='50px'
-          height='50px'
-          alt='Кирпич микс'
-          containerCss={{ m: '0px', pt: '20px', pl: '20px', pb: '0px' }}
-        />
-      </Card.Header>
-      <Card.Body css={{ p: '0 20px 0 20px' }}>
-        <Col>
-          <Text h3 color="gray" css={{ ta: 'center', pb: '0px' }}>
-            Заказ 1 Тольятти
-          </Text>
-          <Text weight="bold">
-            1000шт кирпич Микс
-          </Text>
-          <Text weight="bold">
-            25м2 плитка Микс
-          </Text>
-          <Text weight="bold">
-            212шт. углов Микс
-          </Text>
-        </Col>
-        <Text css={{ d: 'flex', ai: 'center' }}>
-          Статус:
-          <CheckSquareIcon color='green'/>
-          <XSquareIcon color='red'/>
-        </Text>
-      </Card.Body>
-      <Card.Footer css={{ pt: '0px' }}>
-        <Row>
-          <Col>
-            <Text color='gray' css={{ d: 'flex', ai: 'center', jc: 'center' }}>
-              <BankIcon color='grey'/>
-              Оплата: 26.06
-            </Text>
-          </Col>
-          <Col>
-            <Text color='gray' css={{ d: 'flex', ai: 'center', jc: 'center' }}>
-              <CalendarCheckIcon color='grey'/>
-              Срок: 30.06
-            </Text>
-          </Col>
-        </Row>
-      </Card.Footer>
-    </Card>);
+  console.log()
+  const OrderItems = [
+    {
+      number: 1,
+      src: Mix.src,
+      city: 'Тольятти',
+      brick: '1000шт Кирпич микс',
+      tile: '25м2 плитка микс ',
+      corner: '212шт углы микс',
+      dateOfPayment: '26.06',
+      readiness: '30.06'
+    },
+    {
+      number: 2,
+      src: Starina.src,
+      city: 'Самара',
+      brick: '1000шт Кирпич под старину',
+      tile: '25м2 плитка под старину',
+      corner: '212шт углы под старину',
+      dateOfPayment: '08.06',
+      readiness: '20.06'
+    },
+    {
+      number: 2,
+      src: Starina.src,
+      city: 'Самара',
+      brick: '1000шт Кирпич под старину',
+      tile: '25м2 плитка под старину',
+      corner: '212шт углы под старину',
+      dateOfPayment: '08.06',
+      readiness: '20.06'
+    },
+    {
+      number: 2,
+      src: Starina.src,
+      city: 'Самара',
+      brick: '1000шт Кирпич под старину',
+      tile: '25м2 плитка под старину',
+      corner: '212шт углы под старину',
+      dateOfPayment: '08.06',
+      readiness: '20.06'
+    },
+    {
+      number: 2,
+      src: Starina.src,
+      city: 'Самара',
+      brick: '1000шт Кирпич под старину',
+      tile: '25м2 плитка под старину',
+      corner: '212шт углы под старину',
+      dateOfPayment: '08.06',
+      readiness: '20.06'
+    },
+    {
+      number: 2,
+      src: Starina.src,
+      city: 'Самара',
+      brick: '1000шт Кирпич под старину',
+      tile: '25м2 плитка под старину',
+      corner: '212шт углы под старину',
+      dateOfPayment: '08.06',
+      readiness: '20.06'
+    }
+  ]
 
+  return (
+    <Grid.Container>
+      {OrderItems.map((OrderItems) => {
+        return <Grid key={OrderItems.number}><Card css={{ mw: '333px', ml: '80px', mt: '115px' }}>
+          <Card.Header css={{ ai: 'start' }}>
+            <Image
+              src={OrderItems.src}
+              width='50px'
+              height='50px'
+              alt='Кирпич микс'
+              containerCss={{ m: '0px', pt: '20px', pl: '20px', pb: '0px' }}
+            />
+          </Card.Header>
+          <Card.Body css={{ p: '0 20px 0 20px' }}>
+            <Col>
+              <Text h3 color="gray" css={{ ta: 'center', pb: '0px' }}>
+                Заказ: {OrderItems.number} {OrderItems.city}
+              </Text>
+              <Text weight="bold">
+                {OrderItems.brick}
+              </Text>
+              <Text weight="bold">
+                {OrderItems.tile}
+              </Text>
+              <Text weight="bold">
+                {OrderItems.corner}
+              </Text>
+            </Col>
+            <Text css={{ d: 'flex', ai: 'center' }}>
+              Статус:
+              <CheckSquareIcon color='green' />
+              <XSquareIcon color='red' />
+            </Text>
+          </Card.Body>
+          <Card.Footer css={{ pt: '0px' }}>
+            <Row>
+              <Col>
+                <Text color='gray' css={{ d: 'flex', ai: 'center', jc: 'center' }}>
+                  <BankIcon color='grey' />
+                  Оплата: {OrderItems.dateOfPayment}
+                </Text>
+              </Col>
+              <Col>
+                <Text color='gray' css={{ d: 'flex', ai: 'center', jc: 'center' }}>
+                  <CalendarCheckIcon color='grey' />
+                  Срок: {OrderItems.readiness}
+                </Text>
+              </Col>
+            </Row>
+          </Card.Footer>
+        </Card></Grid>
+      })}
+    </Grid.Container>
+  )
 }
+
+
+//AIzaSyA-og7qzrQ-aG8MSDB9jYEMuigHMrr5O2g
