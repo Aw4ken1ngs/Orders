86d4047f9552b59a75735b015b3ff8c6d0de63f7
Vladislav Smirnov <vladislav.smirnov@jetbrains.com>
2024-04-05 22:45:06 +0200

    Added email and pass auth.

--- components/sign-in/sign-in.jsx
+++ components/sign-in/sign-in.jsx
@@ -1,11 +1,54 @@
+import {AutoSuggest} from "@/components/auto-suggest/auto-suggest";
 import React, { useEffect, useState, useMemo, createContext, useContext } from 'react';
-import { Button, ButtonGroup } from "@nextui-org/react";
+import {
+    Button,
+    ButtonGroup,
+    Input,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalHeader, Select, SelectItem,
+    Table, TableBody, TableCell, TableColumn,
+    TableHeader, TableRow, useDisclosure
+} from "@nextui-org/react";
 import { AuthContext } from '@/contexts/auth-context';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword } from "firebase/auth";
 
 export const SingIn = () => {
   const { provider, auth } = useContext(AuthContext);
-  const signIn = () => {
+    const {isOpen, onOpen, onClose, onOpenChange} = useDisclosure();
+    const [form, setForm] = useState({
+        email: '',
+        pass: ''
+    });
+
+    const onChange = (event) => {
+        const {name, value} = event.target;
+
+        setForm({
+            ...form,
+            [name]: value
+        })
+    }
+
+    const signInWithEmail = () => {
+        signInWithEmailAndPassword(auth, form.email, form.pass)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                console.log('user23', user);
+                onClose();
+                // ...
+            })
+            .catch((error) => {
+                console.log('user24', error);
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    }
+
+
+  const signInWithHoohle = () => {
     console.log('signIn');
     signInWithPopup(auth, provider)
       .then((result) => {
@@ -31,5 +74,27 @@ export const SingIn = () => {
         // ...
       });
   }
-  return <Button color='primary' onClick={signIn}>Войти </Button> 
+
+    console.log('form23', form);
+    return (
+      <>
+          <Button color='primary' onClick={onOpen}>Войти</Button>
+          <Modal
+              isOpen={isOpen}
+              onOpenChange={onOpenChange}
+              placement="top-center"
+              size="3xl"
+          >
+              <ModalContent>
+                  <ModalBody>
+                      <Input type="email" name="email" placeholder="Email" onChange={onChange}/>
+                      <Input type="password" name="pass" placeholder="Пароль" onChange={onChange}/>
+                      <Button onClick={signInWithEmail}>Войти</Button>
+                      <br/>
+                      <Button onClick={signInWithHoohle}>Войти через Хухл</Button>
+                          </ModalBody>
+              </ModalContent>
+          </Modal>
+      </>
+  )
 }
